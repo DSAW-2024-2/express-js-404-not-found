b31755bf95dbc27990b38169771d186fdd7bad76
Mao Suarez <maosuarezbarrer@gmail.com>
2024-09-07 17:02:52 -0500

    Parte de Mao

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
@@ -8,12 +9,25 @@ app.get("/user-info/:index", (request, response) => {
   const index = request.params.index;
   const users = [
     {
+      name: "Mauricio",
+      lastName: "Suarez Barrera",
+      email: "mauriciosuba@unisabana.edu.co",
+      id: "0000300562",
+    },
+    {
       name: "Isabela",
       lastName: "Diaz Acosta",
       email: "isabeladiac@unisabana.edu.co",
       id: "0000310594",
     },
   ];
+  try {
+    const user = users[index - 1];
+    response.send(user);
+  } catch (error) {
+    response.status(404);
+    response.send({ message: "User not found" });
+  }
 });
 app.set("port", 3000);
 app.listen("3000", () => {
