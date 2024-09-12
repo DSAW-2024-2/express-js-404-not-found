d273dbffa4c56bf079e056f8d22f3f133572400e
Mao Suarez <maosuarezbarrer@gmail.com>
2024-09-12 17:04:22 -0500

    Dividir En 2 y puerto process.env.PORT

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 const express = require("express");
-const cors = require("cors");
 const app = express();
 
-app.use(cors());
 app.use(express.json());
 
 app.use(require("./routes/index.js"));
 
 app.set("port", process.env.PORT || 3000);
+
 app.listen(app.get("port"), () => {
   console.log("Server is running");
 });
