5ebbd38f773346cdc6cf335bff0ac60c8785db50
isadiazac <isadiac06@gmail.com>
2024-09-13 09:22:46 -0500

    Revert "Update index.js"
    This reverts commit 56278dcf545bef030cb91341cfdd94e38e58abf3.

--- src/netlify/functions/api.mjs
+++ src/netlify/functions/api.mjs
@@ -9,12 +9,6 @@ app.get("/user-info/:index", (request, response) => {
   const index = request.params.index;
   const users = [
     {
-      name: "Mauricio",
-      lastName: "Suarez Barrera",
-      email: "mauriciosuba@unisabana.edu.co",
-      id: "0000300562",
-    },
-    {
       name: "Isabela",
       lastName: "Diaz Acosta",
       email: "isabeladiac@unisabana.edu.co",
